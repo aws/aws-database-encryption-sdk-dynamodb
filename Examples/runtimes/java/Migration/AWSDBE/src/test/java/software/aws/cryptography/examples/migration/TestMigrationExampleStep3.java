23a712d3eca689cc50d7631f9a999539e48122c9
Valerie Lambert <lavaleri@amazon.com>
2023-04-15 12:41:56 -0700

    populate integ table with expected fields

--- Examples/runtimes/java/Migration/AWSDBE/src/test/java/software/aws/cryptography/examples/migration/TestMigrationExampleStep3.java
+++ Examples/runtimes/java/Migration/AWSDBE/src/test/java/software/aws/cryptography/examples/migration/TestMigrationExampleStep3.java
@@ -8,16 +8,6 @@ import static org.testng.Assert.assertThrows;
 public class TestMigrationExampleStep3 {
     @Test
     public void TestMigrationStep3() {
-        // Does not read items from steps 0 and 1
-        assertThrows(SdkClientException.class, () -> {
-            MigrationExampleStep3.MigrationStep3(TestUtils.TEST_KMS_KEY_ID, TestUtils.TEST_DDB_TABLE_NAME, 0);
-        });
-        assertThrows(SdkClientException.class, () -> {
-            MigrationExampleStep3.MigrationStep3(TestUtils.TEST_KMS_KEY_ID, TestUtils.TEST_DDB_TABLE_NAME, 1);
-        });
-
-        // Successfully reads items from steps 2 and 3
-        MigrationExampleStep3.MigrationStep3(TestUtils.TEST_KMS_KEY_ID, TestUtils.TEST_DDB_TABLE_NAME, 2);
         MigrationExampleStep3.MigrationStep3(TestUtils.TEST_KMS_KEY_ID, TestUtils.TEST_DDB_TABLE_NAME, 3);
     }
 }
