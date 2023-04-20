2f708d96b3908c1ff78fa26afae1b5410e1e3cbe
Lucas McDonald <lucmcdon@amazon.com>
2023-04-20 16:53:06 -0700

    changes

--- Examples/runtimes/java/DynamoDbEncryption/src/test/java/software/aws/cryptography/examples/searchableencryption/TestBasicSearchableEncryptionExample.java
+++ Examples/runtimes/java/DynamoDbEncryption/src/test/java/software/aws/cryptography/examples/searchableencryption/TestBasicSearchableEncryptionExample.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class TestBasicSearchableEncryptionExample {
 
   @Test
-  public void TestItemEncryptDecrypt() {
+  public void TestBasicExample() {
     BasicSearchableEncryptionExample.PutItemQueryItemWithBeacon(
         SearchableEncryptionTestUtils.TEST_DDB_TABLE_NAME,
         SearchableEncryptionTestUtils.TEST_BRANCH_KEY_WRAPPING_KMS_KEY_ARN,
