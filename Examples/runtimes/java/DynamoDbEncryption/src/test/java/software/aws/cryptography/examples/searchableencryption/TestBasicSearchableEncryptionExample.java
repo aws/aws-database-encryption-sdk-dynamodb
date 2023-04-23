40af9da237c016140d9cdc4088f95126e95ee250
Valerie Lambert <lavaleri@amazon.com>
2023-04-23 13:27:43 -0700

    fix

--- Examples/runtimes/java/DynamoDbEncryption/src/test/java/software/aws/cryptography/examples/searchableencryption/TestBasicSearchableEncryptionExample.java
+++ Examples/runtimes/java/DynamoDbEncryption/src/test/java/software/aws/cryptography/examples/searchableencryption/TestBasicSearchableEncryptionExample.java
@@ -10,8 +10,8 @@ public class TestBasicSearchableEncryptionExample {
   public void TestBasicExample() throws InterruptedException {
     // Create new branch key for test
     String keyId = CreateKeyStoreKeyExample.KeyStoreCreateKey(
-            TestUtils.TEST_BRANCH_KEYSTORE_DDB_TABLE_NAME,
-            TestUtils.TEST_BRANCH_KEY_WRAPPING_KMS_KEY_ARN);
+            SearchableEncryptionTestUtils.TEST_BRANCH_KEYSTORE_DDB_TABLE_NAME,
+            SearchableEncryptionTestUtils.TEST_BRANCH_KEY_WRAPPING_KMS_KEY_ARN);
 
     // Key creation is eventually consistent, so wait 5 seconds to decrease the likelihood
     // our test fails due to eventual consistency issues.
