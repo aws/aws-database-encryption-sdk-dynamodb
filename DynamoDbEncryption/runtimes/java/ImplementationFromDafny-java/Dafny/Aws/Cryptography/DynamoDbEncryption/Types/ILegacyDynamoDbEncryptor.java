d0c9446627fa3274961dee131c9dc94297ddd559
Valerie Lambert <lavaleri@amazon.com>
2023-05-02 11:49:07 -0700

    whoops

--- DynamoDbEncryption/runtimes/java/ImplementationFromDafny-java/Dafny/Aws/Cryptography/DynamoDbEncryption/Types/ILegacyDynamoDbEncryptor.java
+++
@@ -1,9 +0,0 @@
-// Interface ILegacyDynamoDbEncryptor
-// Dafny trait ILegacyDynamoDbEncryptor compiled into Java
-package Dafny.Aws.Cryptography.DynamoDbEncryption.Types;
-
-import Dafny.Aws.Cryptography.StructuredEncryption.Types.*;
-
-@SuppressWarnings({"unchecked", "deprecation"})
-public interface ILegacyDynamoDbEncryptor {
-}
