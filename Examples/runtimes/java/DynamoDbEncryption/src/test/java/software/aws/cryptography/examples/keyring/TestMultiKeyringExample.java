216eb448fda436ffe015826798b49b2d919b505c
Lucas McDonald <lucmcdon@amazon.com>
2023-05-10 13:56:23 -0700

    update

--- Examples/runtimes/java/DynamoDbEncryption/src/test/java/software/aws/cryptography/examples/keyring/TestMultiKeyringExample.java
+++ Examples/runtimes/java/DynamoDbEncryption/src/test/java/software/aws/cryptography/examples/keyring/TestMultiKeyringExample.java
@@ -1,13 +1,18 @@
 package software.aws.cryptography.examples.keyring;
 
+import java.nio.ByteBuffer;
 import org.testng.annotations.Test;
 import software.aws.cryptography.examples.TestUtils;
 
 public class TestMultiKeyringExample {
     @Test
     public void TestMultiKeyringExample() {
+        // Generate a new AES key
+        ByteBuffer aesKeyBytes = RawAesKeyringExample.generateAesKeyBytes();
+
         MultiKeyringExample.MultiKeyringGetItemPutItem(
                 TestUtils.TEST_DDB_TABLE_NAME,
-                TestUtils.TEST_KMS_KEY_ID);
+                TestUtils.TEST_KMS_KEY_ID,
+                aesKeyBytes);
     }
 }
