b5564b995c21786b4bb86582bcff88ca4fcdf734
Lucas McDonald <lucmcdon@amazon.com>
2023-05-12 10:36:30 -0700

    add discovery example

---
+++ Examples/runtimes/java/DynamoDbEncryption/src/test/java/software/aws/cryptography/examples/keyring/TestMrkDiscoveryMultiKeyringExample.java
@@ -0,0 +1,20 @@
+package software.aws.cryptography.examples.keyring;
+
+import java.util.Collections;
+import java.util.List;
+import org.testng.annotations.Test;
+import software.aws.cryptography.examples.TestUtils;
+
+public class TestMrkDiscoveryMultiKeyringExample {
+    @Test
+    public void TestMrkMultiKeyringExample() {
+        List<String> accounts = Collections.singletonList(TestUtils.TEST_AWS_ACCOUNT_ID);
+        List<String> regions = Collections.singletonList(TestUtils.TEST_AWS_REGION);
+
+        MrkDiscoveryMultiKeyringExample.MultiMrkDiscoveryKeyringGetItemPutItem(
+                TestUtils.TEST_DDB_TABLE_NAME,
+                TestUtils.TEST_MRK_KEY_ID,
+                accounts,
+                regions);
+    }
+}
