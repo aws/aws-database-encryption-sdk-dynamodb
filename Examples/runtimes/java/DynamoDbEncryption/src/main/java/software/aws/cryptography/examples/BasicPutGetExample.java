da5cb991fa654e76bbcbc0ea5ea1d7a817103775
Lucas McDonald <lucmcdon@amazon.com>
2023-05-16 09:22:49 -0700

    update 3

--- Examples/runtimes/java/DynamoDbEncryption/src/main/java/software/aws/cryptography/examples/BasicPutGetExample.java
+++ Examples/runtimes/java/DynamoDbEncryption/src/main/java/software/aws/cryptography/examples/BasicPutGetExample.java
@@ -8,6 +8,7 @@ import software.amazon.cryptography.dbencryptionsdk.dynamodb.model.DynamoDbTable
 import software.amazon.cryptography.materialproviders.IKeyring;
 import software.amazon.cryptography.materialproviders.MaterialProviders;
 import software.amazon.cryptography.materialproviders.model.CreateAwsKmsMrkMultiKeyringInput;
+import software.amazon.cryptography.materialproviders.model.DBEAlgorithmSuiteId;
 import software.amazon.cryptography.materialproviders.model.MaterialProvidersConfig;
 import software.amazon.cryptography.dbencryptionsdk.structuredencryption.model.CryptoAction;
 import software.aws.cryptography.dbencryptionsdk.dynamodb.DynamoDbEncryptionInterceptor;
@@ -95,6 +96,18 @@ public class BasicPutGetExample {
                 .attributeActions(attributeActions)
                 .keyring(kmsKeyring)
                 .allowedUnauthenticatedAttributePrefix(unauthAttrPrefix)
+                // Specifying an algorithm suite is not required,
+                // but is done here to demonstrate how to do so.
+                // We suggest using the
+                // `ALG_AES_256_GCM_HKDF_SHA512_COMMIT_KEY_ECDSA_P384_SYMSIG_HMAC_SHA384` suite,
+                // which includes AES-GCM with key derivation, signing, and key commitment.
+                // This is also the default algorithm suite if one is not specified in this config.
+                // For more information on supported algorithm suites, see
+                //   TODO: Add DB ESDK-specific link, similar to
+                //   https://docs.aws.amazon.com/encryption-sdk/latest/developer-guide/supported-algorithms.html,
+                //   but with accurate information for DB ESDK
+                .algorithmSuiteId(
+                    DBEAlgorithmSuiteId.ALG_AES_256_GCM_HKDF_SHA512_COMMIT_KEY_ECDSA_P384_SYMSIG_HMAC_SHA384)
                 .build();
         tableConfigs.put(ddbTableName, config);
 
