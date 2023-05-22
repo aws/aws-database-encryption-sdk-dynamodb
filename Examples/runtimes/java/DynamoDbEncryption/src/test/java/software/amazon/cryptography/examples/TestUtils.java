e9a2a51fa064643986c93fa2b0f832e64430508d
Lucas McDonald <lucasmcdonald3@gmail.com>
2023-05-22 12:56:58 -0700

    feat(Examples): Create KMS RSA keyring example (#188)

--- Examples/runtimes/java/DynamoDbEncryption/src/test/java/software/amazon/cryptography/examples/TestUtils.java
+++ Examples/runtimes/java/DynamoDbEncryption/src/test/java/software/amazon/cryptography/examples/TestUtils.java
@@ -10,9 +10,9 @@ public class TestUtils {
     // These are public KMS Keys that MUST only be used for testing, and MUST NOT be used for any production data
     public static final String TEST_KMS_KEY_ID = "arn:aws:kms:us-west-2:658956600833:key/b3537ef1-d8dc-4780-9f5a-55776cbb2f7f";
     public static final String TEST_MRK_KEY_ID = "arn:aws:kms:us-west-2:658956600833:key/mrk-80bd8ecdcd4342aebd84b7dc9da498a7";
+    public static final String TEST_KMS_RSA_KEY_ID = "arn:aws:kms:us-west-2:658956600833:key/8b432da4-dde4-4bc3-a794-c7d68cbab5a6";
     public static final String TEST_MRK_REPLICA_KEY_ID_US_EAST_1 = "arn:aws:kms:us-east-1:658956600833:key/mrk-80bd8ecdcd4342aebd84b7dc9da498a7";
     public static final String TEST_MRK_REPLICA_KEY_ID_EU_WEST_1 = "arn:aws:kms:eu-west-1:658956600833:key/mrk-80bd8ecdcd4342aebd84b7dc9da498a7";
-
 
     // Our tests require access to DDB Table with this name
     public static final String TEST_DDB_TABLE_NAME = "DynamoDbEncryptionInterceptorTestTable";
