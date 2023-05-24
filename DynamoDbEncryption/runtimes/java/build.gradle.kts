2dd37bf3811f53406e3bb3b06c6f750394b248ca
Robin Salkeld <salkeldr@amazon.com>
2023-05-23 17:39:48 -0700

    Remove unneeded CodeArtifact setup, fix version number, update submodules (#213)
    Co-authored-by: Lucas McDonald <lucasmcdonald3@gmail.com>

--- DynamoDbEncryption/runtimes/java/build.gradle.kts
+++ DynamoDbEncryption/runtimes/java/build.gradle.kts
@@ -65,7 +65,7 @@ val dynamodb by configurations.creating
 
 dependencies {
     implementation("org.dafny:DafnyRuntime:4.1.0")
-    implementation("software.amazon.smithy.dafny:conversion:1.0-SNAPSHOT")
+    implementation("software.amazon.smithy.dafny:conversion:0.1")
     implementation("software.amazon.cryptography:AwsCryptographicMaterialProviders:1.0-SNAPSHOT")
 
     implementation(platform("software.amazon.awssdk:bom:2.19.1"))
