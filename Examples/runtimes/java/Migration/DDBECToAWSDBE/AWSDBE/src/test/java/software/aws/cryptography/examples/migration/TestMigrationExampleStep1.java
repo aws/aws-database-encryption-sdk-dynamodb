eb4dbcad7c8a844146c6b2560cb6add56ccdb088
Lucas McDonald <lucmcdon@amazon.com>
2023-05-26 14:15:18 -0700

    Revert "try this"
    This reverts commit accb9f137b096ad0fc59b4e3d6b428e3c18f80e2.

---
+++ Examples/runtimes/java/Migration/DDBECToAWSDBE/AWSDBE/src/test/java/software/aws/cryptography/examples/migration/TestMigrationExampleStep1.java
@@ -0,0 +1,13 @@
+package software.amazon.cryptography.examples.migration;
+
+import org.testng.annotations.Test;
+
+public class TestMigrationExampleStep1 {
+    @Test
+    public void TestMigrationStep1() {
+        MigrationExampleStep1.MigrationStep1(TestUtils.TEST_KMS_KEY_ID, TestUtils.TEST_DDB_TABLE_NAME, 0);
+        MigrationExampleStep1.MigrationStep1(TestUtils.TEST_KMS_KEY_ID, TestUtils.TEST_DDB_TABLE_NAME, 1);
+        MigrationExampleStep1.MigrationStep1(TestUtils.TEST_KMS_KEY_ID, TestUtils.TEST_DDB_TABLE_NAME, 2);
+        MigrationExampleStep1.MigrationStep1(TestUtils.TEST_KMS_KEY_ID, TestUtils.TEST_DDB_TABLE_NAME, 3);
+    }
+}
---
+++ Examples/runtimes/java/Migration/DDBECToAWSDBE/DDBEC/src/README.md
@@ -0,0 +1,13 @@
+# DyanmoDb Encryption Client to AWS Database Encryption SDK for DynamoDb Migration
+
+This project represents the starting state for migration to the AWS Database Encryption SDK from the
+DynamoDb Encryption Client.
+
+## Step 0
+
+This project contains an example that demonstrates putting and getting items into DynamoDb
+using the DynamoDBMapper configured with a DirectKmsMaterialProvider.
+
+This project is using the AWS SDK for Java v1.
+
+To start the migration, see [Step 1](../AWSDBE/README.md#step-1).
