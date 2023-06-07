1889e312bb63cf4c78d0c266149f8e8a1b8b2233
Lucas McDonald <lucasmcdonald3@gmail.com>
2023-06-06 17:44:13 -0700

    feat(Examples): Finish workshop example (#216)

--- Examples/runtimes/java/DynamoDbEncryption/src/main/java/software/amazon/cryptography/examples/searchableencryption/complexexample/ComplexSearchableEncryptionExample.java
+++ Examples/runtimes/java/DynamoDbEncryption/src/main/java/software/amazon/cryptography/examples/searchableencryption/complexexample/ComplexSearchableEncryptionExample.java
@@ -4,6 +4,16 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 public class ComplexSearchableEncryptionExample {
 
+  /*
+   * This file is used in an example to demonstrate complex queries
+   * you can perform using beacons.
+   * The example data used is for demonstrative purposes only,
+   * and might not meet the distribution and correlation uniqueness
+   * recommendations for beacons.
+   * See our documentation for whether beacons are
+   * right for your particular data set:
+   * https://docs.aws.amazon.com/database-encryption-sdk/latest/devguide/searchable-encryption.html#are-beacons-right-for-me
+   */
   public static void runExample(String ddbTableName, String branchKeyId, String branchKeyWrappingKmsKeyArn, String branchKeyDdbTableName) {
     DynamoDbClient ddb = BeaconConfig.SetupBeaconConfig(ddbTableName, branchKeyId, branchKeyWrappingKmsKeyArn, branchKeyDdbTableName);
     PutRequests.putAllItemsToTable(ddbTableName, ddb);
