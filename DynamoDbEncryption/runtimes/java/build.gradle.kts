db5031f998a9c3dd3da0520b0e6cdc6cc8ea1530
Jose Corella <joseeduardocorella@gmail.com>
2023-06-07 23:15:29 -0700

    add release prod

--- DynamoDbEncryption/runtimes/java/build.gradle.kts
+++ DynamoDbEncryption/runtimes/java/build.gradle.kts
@@ -241,6 +241,8 @@ nexusPublishing {
     // https://github.com/gradle-nexus/publish-plugin/
     repositories {
         sonatype {
+            nexusUrl.set(uri("https://aws.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://aws.oss.sonatype.org/content/repositories/snapshots/"))
             username.set(System.getenv("SONA_USERNAME"))
             password.set(System.getenv("SONA_PASSWORD"))
         }
