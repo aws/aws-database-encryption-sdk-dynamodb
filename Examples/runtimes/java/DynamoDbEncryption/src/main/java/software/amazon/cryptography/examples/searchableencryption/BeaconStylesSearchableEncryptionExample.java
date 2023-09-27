a52dd63efa192d8b771407dff8f8723454b617cc
Andrew Jewell <107044381+ajewellamz@users.noreply.github.com>
2023-09-27 13:31:36 -0400

    Update Examples/runtimes/java/DynamoDbEncryption/src/main/java/software/amazon/cryptography/examples/searchableencryption/BeaconStylesSearchableEncryptionExample.java
    Co-authored-by: Lucas McDonald <lucasmcdonald3@gmail.com>

--- Examples/runtimes/java/DynamoDbEncryption/src/main/java/software/amazon/cryptography/examples/searchableencryption/BeaconStylesSearchableEncryptionExample.java
+++ Examples/runtimes/java/DynamoDbEncryption/src/main/java/software/amazon/cryptography/examples/searchableencryption/BeaconStylesSearchableEncryptionExample.java
@@ -293,10 +293,10 @@ public class BeaconStylesSearchableEncryptionExample {
     scanResponse = ddb.scan(scanRequest);
 
     // Validate query was returned successfully
-	assert 200 == scanResponse.sdkHttpResponse().statusCode();
+    assert 200 == scanResponse.sdkHttpResponse().statusCode();
 
-	// Validate only 1 item was returned: item2
-	assert scanResponse.items().size() == 1;
+    // Validate only 1 item was returned: item2
+    assert scanResponse.items().size() == 1;
     assert scanResponse.items().get(0).equals(item2);
 
     // Test a Twinned search. Select records where the dessert attribute matches the fruit attribute
