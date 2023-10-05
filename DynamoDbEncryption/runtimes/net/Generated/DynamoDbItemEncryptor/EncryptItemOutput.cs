00899ee6ff80d9a99642663f260019854394ee62
Andrew Jewell <107044381+ajewellamz@users.noreply.github.com>
2023-10-05 12:51:07 -0400

    feat: repair dotnet build (#466)
    * feat: repair dotnet build, enable net ci

---
+++ DynamoDbEncryption/runtimes/net/Generated/DynamoDbItemEncryptor/EncryptItemOutput.cs
@@ -0,0 +1,36 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+// Do not modify this file. This file is machine generated, and any changes to it will be overwritten.
+using System;
+using AWS.Cryptography.DbEncryptionSDK.DynamoDb.ItemEncryptor;
+namespace AWS.Cryptography.DbEncryptionSDK.DynamoDb.ItemEncryptor
+{
+  public class EncryptItemOutput
+  {
+    private System.Collections.Generic.Dictionary<string, Amazon.DynamoDBv2.Model.AttributeValue> _encryptedItem;
+    private AWS.Cryptography.DbEncryptionSDK.DynamoDb.ItemEncryptor.ParsedHeader _parsedHeader;
+    public System.Collections.Generic.Dictionary<string, Amazon.DynamoDBv2.Model.AttributeValue> EncryptedItem
+    {
+      get { return this._encryptedItem; }
+      set { this._encryptedItem = value; }
+    }
+    public bool IsSetEncryptedItem()
+    {
+      return this._encryptedItem != null;
+    }
+    public AWS.Cryptography.DbEncryptionSDK.DynamoDb.ItemEncryptor.ParsedHeader ParsedHeader
+    {
+      get { return this._parsedHeader; }
+      set { this._parsedHeader = value; }
+    }
+    public bool IsSetParsedHeader()
+    {
+      return this._parsedHeader != null;
+    }
+    public void Validate()
+    {
+      if (!IsSetEncryptedItem()) throw new System.ArgumentException("Missing value for required property 'EncryptedItem'");
+
+    }
+  }
+}
---
+++ DynamoDbEncryption/runtimes/net/Generated/StructuredEncryption/ParsedHeader.cs
@@ -0,0 +1,59 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+// Do not modify this file. This file is machine generated, and any changes to it will be overwritten.
+using System;
+using AWS.Cryptography.DbEncryptionSDK.StructuredEncryption;
+namespace AWS.Cryptography.DbEncryptionSDK.StructuredEncryption
+{
+  public class ParsedHeader
+  {
+    private AWS.Cryptography.DbEncryptionSDK.StructuredEncryption.CryptoSchema _cryptoSchema;
+    private AWS.Cryptography.MaterialProviders.DBEAlgorithmSuiteId _algorithmSuiteId;
+    private System.Collections.Generic.List<AWS.Cryptography.MaterialProviders.EncryptedDataKey> _encryptedDataKeys;
+    private System.Collections.Generic.Dictionary<string, string> _storedEncryptionContext;
+    public AWS.Cryptography.DbEncryptionSDK.StructuredEncryption.CryptoSchema CryptoSchema
+    {
+      get { return this._cryptoSchema; }
+      set { this._cryptoSchema = value; }
+    }
+    public bool IsSetCryptoSchema()
+    {
+      return this._cryptoSchema != null;
+    }
+    public AWS.Cryptography.MaterialProviders.DBEAlgorithmSuiteId AlgorithmSuiteId
+    {
+      get { return this._algorithmSuiteId; }
+      set { this._algorithmSuiteId = value; }
+    }
+    public bool IsSetAlgorithmSuiteId()
+    {
+      return this._algorithmSuiteId != null;
+    }
+    public System.Collections.Generic.List<AWS.Cryptography.MaterialProviders.EncryptedDataKey> EncryptedDataKeys
+    {
+      get { return this._encryptedDataKeys; }
+      set { this._encryptedDataKeys = value; }
+    }
+    public bool IsSetEncryptedDataKeys()
+    {
+      return this._encryptedDataKeys != null;
+    }
+    public System.Collections.Generic.Dictionary<string, string> StoredEncryptionContext
+    {
+      get { return this._storedEncryptionContext; }
+      set { this._storedEncryptionContext = value; }
+    }
+    public bool IsSetStoredEncryptionContext()
+    {
+      return this._storedEncryptionContext != null;
+    }
+    public void Validate()
+    {
+      if (!IsSetCryptoSchema()) throw new System.ArgumentException("Missing value for required property 'CryptoSchema'");
+      if (!IsSetAlgorithmSuiteId()) throw new System.ArgumentException("Missing value for required property 'AlgorithmSuiteId'");
+      if (!IsSetEncryptedDataKeys()) throw new System.ArgumentException("Missing value for required property 'EncryptedDataKeys'");
+      if (!IsSetStoredEncryptionContext()) throw new System.ArgumentException("Missing value for required property 'StoredEncryptionContext'");
+
+    }
+  }
+}
