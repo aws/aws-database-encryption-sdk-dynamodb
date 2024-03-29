92751512f6e004cfd83e8fff85a1827df42c8f08
rishav-karanjit <karanjitrishav4@gmail.com>
2024-03-29 09:08:29 -0700

    Add comments to dotnet example

--- Examples/runtimes/net/src/GetEncryptedDataKeyDescriptionExample.cs
+++ Examples/runtimes/net/src/GetEncryptedDataKeyDescriptionExample.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using Amazon.DynamoDBv2.Model;
 using Amazon.DynamoDBv2;
 using AWS.Cryptography.DbEncryptionSDK.DynamoDb;
-using Amazon.Runtime;
 
 public class GetEncryptedDataKeyDescriptionExample
 {
@@ -14,54 +13,39 @@ public class GetEncryptedDataKeyDescriptionExample
     {
         var kmsKeyId = TestUtils.TEST_KMS_KEY_ID;
         var ddbTableName = TestUtils.TEST_DDB_TABLE_NAME;
-
         var ddbEnc = new DynamoDbEncryption(new DynamoDbEncryptionConfig());
-
         string header_column = "aws_dbe_head";
 
+        // 1. Define keys that will be used to retrieve item from the DynamoDB table.
         var keyToGet = new Dictionary<String, AttributeValue>
         {
             ["partition_key"] = new AttributeValue("BasicPutGetExample"),
             ["sort_key"] = new AttributeValue { N = "0" },
         };
         
-        // 4. Create the DynamoDb Encryption configuration for the table we will be writing to.
-        Dictionary<String, DynamoDbTableEncryptionConfig> tableConfigs =
-            new Dictionary<String, DynamoDbTableEncryptionConfig>();
-        DynamoDbTableEncryptionConfig config = new DynamoDbTableEncryptionConfig
-        {
-            LogicalTableName = ddbTableName,
-            PartitionKeyName = "partition_key",
-            SortKeyName = "sort_key",
-        };
-        tableConfigs.Add(ddbTableName, config);
-        
-        // 5. Create a new AWS SDK DynamoDb client using the TableEncryptionConfigs
+        // 2. Create a Amazon DynamoDB Client and retrieve item from DynamoDB table
         var ddb = new AmazonDynamoDBClient();
-
         GetItemRequest getRequest = new GetItemRequest
         {
             Key = keyToGet,
             TableName = ddbTableName,
         };
-
         GetItemResponse getResponse = await ddb.GetItemAsync(getRequest);
 
+        // Demonstrate that PutItem succeeded
         Debug.Assert(getResponse.HttpStatusCode == HttpStatusCode.OK);
+
+        // 3. Extract the item from the dynamoDB table and prepare input for the GetEncryptedDataKeyDescription method.
+        // Here, we are sending header as the input by getting it from "aws_dbe_head" attribute but you can send a complete DDB item as well.
         var returnedItem = getResponse.Item;
-        
         GetEncryptedDataKeyDescriptionUnion InputUnion = new GetEncryptedDataKeyDescriptionUnion();
-
         InputUnion.Header = returnedItem[header_column].B;
-
         GetEncryptedDataKeyDescriptionInput Input = new GetEncryptedDataKeyDescriptionInput();
-
         Input.Input = InputUnion;
-
         GetEncryptedDataKeyDescriptionOutput output = ddbEnc.GetEncryptedDataKeyDescription(Input);
 
+        // 4. Get encrypted DataKey Descriptions from GetEncryptedDataKeyDescription method output and assert if its true.
         var encryptedDataKeyDescriptions = output.EncryptedDataKeyDescriptionOutput;
-
         Debug.Assert(encryptedDataKeyDescriptions[0].KeyProviderId == "aws-kms");
         Debug.Assert(encryptedDataKeyDescriptions[0].KeyProviderInfo == kmsKeyId);        
     }
