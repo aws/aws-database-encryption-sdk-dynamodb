31d68c9ddfefca0d4ec097553b13dac10abbdb6b
Mikael Mayer <mimayere@amazon.com>
2024-05-31 17:05:27 -0500

    Updated beneration w.r.t. feat-rust till line 60916

--- submodules/runtimes/rust/reorder.js
+++ submodules/runtimes/rust/reorder.js
@@ -19,18 +19,23 @@ const uncompilableCorrectOrder = process.argv[3];
 const outputFile = process.argv[4];
 const groundTruthWrongOrderContent = fs.readFileSync(groundTruthWrongOrder).toString();
 const uncompilableCorrectOrderContent = fs.readFileSync(uncompilableCorrectOrder).toString();
-const groundTruthWrongOrderModules = groundTruthWrongOrderContent.split(/^(?=pub mod )/);
-const uncompilableCorrectOrderModules = uncompilableCorrectOrderContent.split(/^(?=pub mod )/);
+const groundTruthWrongOrderModules = splitAtPubMod(groundTruthWrongOrderContent);//.split(/^(?=pub mod )/);
+const uncompilableCorrectOrderModules = splitAtPubMod(uncompilableCorrectOrderContent);//.split(/^(?=pub mod )/);
+console.log("Number of modules detected: ", uncompilableCorrectOrderModules.length)
 // Everything that is not pub mode is assigned 0
 let positionInUncompilable = {};
 let i = 0;
+console.log("Order of modules:");
 for(let module of uncompilableCorrectOrderModules) {
   // We need to extract the potential module name
   const r = /pub mod (\S+)/;
   var match;
   if(match = module.match(r)) {
     const moduleName = match[1];
+    console.log(i + ":" + moduleName);
     positionInUncompilable[moduleName] = i;
+  } else {
+    console.log(i + ":No match");
   }
   i++;
 }
@@ -49,6 +54,19 @@ groundTruthWrongOrderModules.sort((module1, module2) => {
     }
     return 0;
 });
+
+// Given a string that contains "\npub mod", splits so that the second string starts with "pub mod"
+// Do it recursively until there is no more "\npub mod"
+function splitAtPubMod(str) {
+  var result = [str];
+  while(result[result.length - 1].includes("\npub mod")) {
+    var s = result[result.length - 1];
+    var i = s.indexOf("\npub mod");
+    result[result.length - 1] = s.slice(0, i + 1);
+    result.push(s.slice(i + 1));
+  }
+  return result;
+}
 
 // Now emit the code to the output file
 const outputContent = groundTruthWrongOrderModules.join("");
