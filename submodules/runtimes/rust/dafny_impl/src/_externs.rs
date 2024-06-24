88b15a4400584e500877dc10ce500d7827085a45
Mikael Mayer <mimayere@amazon.com>
2024-06-24 11:06:33 -0500

    Modified what the expected generated output should look like

--- submodules/runtimes/rust/dafny_impl/src/_externs.rs
+++ submodules/runtimes/rust/dafny_impl/src/_externs.rs
@@ -145,18 +145,25 @@ pub mod DafnyLibraries {
   }
 }
 
-impl r#_Dafny_dFileIO::_default {
-  pub fn r#_INTERNAL_ReadBytesFromFile(_file: &::dafny_runtime::Sequence<::dafny_runtime::DafnyCharUTF16>)
-    -> (bool, ::dafny_runtime::Sequence<u8>, ::dafny_runtime::Sequence<::dafny_runtime::DafnyCharUTF16>)
-  {
-    todo!("r#_Dafny_dFileIO::r#_INTERNAL_ReadBytesFromFile not implemented");
-  }
-  pub fn r#_INTERNAL_WriteBytesToFile(_path: &::dafny_runtime::Sequence<::dafny_runtime::DafnyCharUTF16>, _bytes: &::dafny_runtime::Sequence<u8>)
-    -> (bool, ::dafny_runtime::Sequence<::dafny_runtime::DafnyCharUTF16>)
-  {
-    todo!("r#_Dafny_dFileIO::(path: &::dafny_runtime::Sequence<::dafny_runtime::DafnyCharUTF16>, bytes: &::dafny_runtime::Sequence<u8>) not implemented");
+pub mod r#_DafnyLibraries_dFileIO {
+  
+  #[allow(non_camel_case)]
+  pub struct _default {}
+  
+  impl _default {
+    pub fn r#_INTERNAL_ReadBytesFromFile(_file: &::dafny_runtime::Sequence<::dafny_runtime::DafnyCharUTF16>)
+      -> (bool, ::dafny_runtime::Sequence<u8>, ::dafny_runtime::Sequence<::dafny_runtime::DafnyCharUTF16>)
+    {
+      todo!("r#_Dafny_dFileIO::r#_INTERNAL_ReadBytesFromFile not implemented");
+    }
+    pub fn r#_INTERNAL_WriteBytesToFile(_path: &::dafny_runtime::Sequence<::dafny_runtime::DafnyCharUTF16>, _bytes: &::dafny_runtime::Sequence<u8>)
+      -> (bool, ::dafny_runtime::Sequence<::dafny_runtime::DafnyCharUTF16>)
+    {
+      todo!("r#_Dafny_dFileIO::(path: &::dafny_runtime::Sequence<::dafny_runtime::DafnyCharUTF16>, bytes: &::dafny_runtime::Sequence<u8>) not implemented");
+    }
   }
 }
+
 
 pub mod Signature_dECDSA {
   use crate::ImplementationFromDafny::*;
