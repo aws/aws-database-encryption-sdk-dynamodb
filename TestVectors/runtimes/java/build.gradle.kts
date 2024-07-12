2f3ece37d7459b3a0ef8713bad1bb3d55ac86fc5
Jose Corella <joseeduardocorella@gmail.com>
2024-07-11 17:14:51 -0700

    update

--- TestVectors/runtimes/java/build.gradle.kts
+++ TestVectors/runtimes/java/build.gradle.kts
@@ -28,6 +28,7 @@ var dafnyRuntimeJavaVersion = props.getProperty("dafnyRuntimeJavaVersion")
 var smithyDafnyJavaConversionVersion = props.getProperty("smithyDafnyJavaConversionVersion")
 
 group = "software.amazon.cryptography"
+// change to ${mplVersion} for next MPL update
 version = "1.0-SNAPSHOT"
 description = "TestVectorsDynamoDbEncryption"
 
@@ -90,7 +91,8 @@ dependencies {
     implementation("software.amazon.smithy.dafny:conversion:${smithyDafnyJavaConversionVersion}")
     implementation("software.amazon.cryptography:aws-cryptographic-material-providers:${mplVersion}")
     implementation("software.amazon.cryptography:aws-database-encryption-sdk-dynamodb:${ddbecVersion}")
-    implementation("software.amazon.cryptography:TestAwsCryptographicMaterialProviders:${mplVersion}")
+    // change to ${mplVersion} for next MPL update
+    implementation("software.amazon.cryptography:TestAwsCryptographicMaterialProviders:1.0-SNAPSHOT")
 
     implementation(platform("software.amazon.awssdk:bom:2.24.2"))
     implementation("software.amazon.awssdk:dynamodb")
