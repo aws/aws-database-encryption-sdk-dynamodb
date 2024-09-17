b4f75042b59a14adf539cfb0d1cd73a658917d6e
Andy Jewell <ajewell@amazon.com>
2024-09-17 11:52:49 -0400

    feat: add rust support

---
+++ DynamoDbEncryption/runtimes/rust/repair_build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -eu
+
+cd $( dirname -- "${BASH_SOURCE[0]}" )
+
+cat extra_ddb.txt >> src/deps/com_amazonaws_dynamodb/client.rs
+cat extra_kms.txt >> src/deps/com_amazonaws_kms/client.rs
+
+cp extra_impl src/new_implementation_from_dafny.rs
+tail +3 src/implementation_from_dafny.rs >> src/new_implementation_from_dafny.rs
+mv src/new_implementation_from_dafny.rs src/implementation_from_dafny.rs
