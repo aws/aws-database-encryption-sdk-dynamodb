c7ca89490eed423b316a5507e127dbf5469e0e2f
Andy Jewell <ajewell@amazon.com>
2024-09-17 12:13:22 -0400

    m

--- DynamoDbEncryption/runtimes/rust/repair_build.sh
+++ DynamoDbEncryption/runtimes/rust/repair_build.sh
@@ -5,6 +5,6 @@ cd $( dirname -- "${BASH_SOURCE[0]}" )
 cat extra_ddb.txt >> src/deps/com_amazonaws_dynamodb/client.rs
 cat extra_kms.txt >> src/deps/com_amazonaws_kms/client.rs
 
-cp extra_impl src/new_implementation_from_dafny.rs
+cp extra_impl.txt src/new_implementation_from_dafny.rs
 tail +3 src/implementation_from_dafny.rs >> src/new_implementation_from_dafny.rs
 mv src/new_implementation_from_dafny.rs src/implementation_from_dafny.rs
