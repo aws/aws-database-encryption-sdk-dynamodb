97dde01a3b6d9a33a8b60dcb6e63debc5329d691
Andrew Jewell <107044381+ajewellamz@users.noreply.github.com>
2024-12-04 09:13:45 -0500

    chore: add Rust release directory (#1479)
    * chore: add Rust release directory

---
+++ releases/rust/db_esdk/examples/clientsupplier/mod.rs
@@ -0,0 +1,6 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod client_supplier_example;
+pub mod regional_role_client_supplier;
+pub mod regional_role_client_supplier_config;
