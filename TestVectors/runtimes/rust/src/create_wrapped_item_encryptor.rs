8074ccdf5c6010e8751063ba2604315417ea4b22
Lucas McDonald <lucmcdon@amazon.com>
2025-04-17 15:17:23 -0700

    m

--- TestVectors/runtimes/rust/src/create_wrapped_item_encryptor.rs
+++ TestVectors/runtimes/rust/src/create_wrapped_item_encryptor.rs
@@ -15,9 +15,9 @@ impl _CreateWrappedItemEncryptor_Compile::_default {
     pub fn CreateWrappedItemEncryptor(
         config: &Rc<crate::implementation_from_dafny::software::amazon::cryptography::dbencryptionsdk::dynamodb::itemencryptor::internaldafny::types::DynamoDbItemEncryptorConfig>
     ) -> Rc<_Wrappers_Compile::Result<Object<dyn IDynamoDbItemEncryptorClient>, Rc<Error>>> {
-        let native_config = crate::deps::aws_cryptography_dbEncryptionSdk_dynamoDb_transforms::conversions::dynamo_db_item_encryptor_config::_dynamo_db_item_encryptor_config::plain_from_dafny(config);
+        let native_config = crate::deps::aws_cryptography_dbEncryptionSdk_dynamoDb_itemencryptor::conversions::dynamo_db_item_encryptor_config::_dynamo_db_item_encryptor_config::plain_from_dafny(config);
         
-        let item_encryptor = aws_cryptography_dbencryptionsdk_dynamodb::item_encryptor::DynamoDbItemEncryptor::new(native_config);
+        let item_encryptor = aws_db_esdk::item_encryptor::Client::from_conf(native_config);
         let wrapped_encryptor = crate::deps::com_amazonaws_dynamodb::item_encryptor::DynamoDbItemEncryptorShim { inner: item_encryptor };
         let dafny_encryptor = ::dafny_runtime::upcast_object()(::dafny_runtime::object::new(wrapped_encryptor));
         
