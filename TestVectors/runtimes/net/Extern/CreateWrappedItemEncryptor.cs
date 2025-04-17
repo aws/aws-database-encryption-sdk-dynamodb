d3ac5fe9b714257df857669b1f18d257aa884a0f
Lucas McDonald <lucmcdon@amazon.com>
2025-04-17 13:20:45 -0700

    m

--- TestVectors/runtimes/net/Extern/CreateWrappedItemEncryptor.cs
+++ TestVectors/runtimes/net/Extern/CreateWrappedItemEncryptor.cs
@@ -1,5 +1,6 @@
 using AWS.Cryptography.DbEncryptionSDK.DynamoDb.ItemEncryptor;
 using AWS.Cryptography.DbEncryptionSDK.DynamoDb;
+using AWS.Cryptography.DbEncryptionSDK.DynamoDb.ItemEncryptor.Wrapped;
 using software.amazon.cryptography.services.dynamodb.internaldafny;
 using software.amazon.cryptography.services.dynamodb.internaldafny.types;
 using Wrappers_Compile;
@@ -12,12 +13,12 @@ namespace CreateWrappedItemEncryptor_Compile
         public static _IResult<software.amazon.cryptography.dbencryptionsdk.dynamodb.itemencryptor.internaldafny.types.IDynamoDbItemEncryptorClient, _IError> CreateWrappedItemEncryptor(
             software.amazon.cryptography.dbencryptionsdk.dynamodb.itemencryptor.internaldafny.types._IDynamoDbItemEncryptorConfig config)
         {
-            var nativeConfig = AWS.Cryptography.DbEncryptionSDK.DynamoDb.Transforms.TypeConversion
-                .FromDafny_N3_aws__N12_cryptography__N15_dbEncryptionSdk__N8_dynamoDb__S30_DynamoDbItemEncryptorConfig(
+            var nativeConfig = AWS.Cryptography.DbEncryptionSDK.DynamoDb.ItemEncryptor.TypeConversion
+                .FromDafny_N3_aws__N12_cryptography__N15_dbEncryptionSdk__N8_dynamoDb__N13_itemEncryptor__S27_DynamoDbItemEncryptorConfig(
                     config);
 
-            var itemEncryptor = DynamoDbItemEncryptor.Create(nativeConfig);
-            var wrappedEncryptor = new Com.Amazonaws.Dynamodb.ItemEncryptorShim(itemEncryptor);
+            var itemEncryptor = new DynamoDbItemEncryptor(nativeConfig);
+            var wrappedEncryptor = new DynamoDbItemEncryptorShim(itemEncryptor);
 
             return new Result_Success<IItemEncryptor, _IError>(wrappedEncryptor);
         }
