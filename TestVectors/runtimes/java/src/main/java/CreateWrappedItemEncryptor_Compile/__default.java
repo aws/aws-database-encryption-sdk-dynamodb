afd61ec233b7cd16a99987b6256ee22ba7009cac
Lucas McDonald <lucmcdon@amazon.com>
2025-04-17 15:04:34 -0700

    mm

--- TestVectors/runtimes/java/src/main/java/CreateWrappedItemEncryptor_Compile/__default.java
+++ TestVectors/runtimes/java/src/main/java/CreateWrappedItemEncryptor_Compile/__default.java
@@ -2,31 +2,32 @@ package CreateWrappedItemEncryptor_Compile;
 
 import Wrappers_Compile.Result;
 import software.amazon.cryptography.dbencryptionsdk.dynamodb.itemencryptor.DynamoDbItemEncryptor;
-import software.amazon.cryptography.dbencryptionsdk.dynamodb.itemencryptor.model.DynamoDbItemEncryptorConfig;
 import software.amazon.cryptography.dbencryptionsdk.dynamodb.itemencryptor.ToDafny;
 import software.amazon.cryptography.dbencryptionsdk.dynamodb.itemencryptor.ToNative;
-import software.amazon.cryptography.dbencryptionsdk.dynamodb.itemencryptor.internaldafny.types.IDynamoDbItemEncryptorClient;
 import software.amazon.cryptography.dbencryptionsdk.dynamodb.itemencryptor.internaldafny.types.Error;
+import software.amazon.cryptography.dbencryptionsdk.dynamodb.itemencryptor.internaldafny.types.IDynamoDbItemEncryptorClient;
+import software.amazon.cryptography.dbencryptionsdk.dynamodb.itemencryptor.model.DynamoDbItemEncryptorConfig;
 import software.amazon.cryptography.dbencryptionsdk.dynamodb.itemencryptor.wrapped.TestDynamoDbItemEncryptor;
 
 public class __default {
 
-  public static Result<IDynamoDbItemEncryptorClient, Error> CreateWrappedItemEncryptor(
+  public static Result<
+    IDynamoDbItemEncryptorClient,
+    Error
+  > CreateWrappedItemEncryptor(
     software.amazon.cryptography.dbencryptionsdk.dynamodb.itemencryptor.internaldafny.types.DynamoDbItemEncryptorConfig config
   ) {
     try {
       final DynamoDbItemEncryptorConfig nativeConfig =
         ToNative.DynamoDbItemEncryptorConfig(config);
 
-      final DynamoDbItemEncryptor itemEncryptor = DynamoDbItemEncryptor
-        .builder()
-        .DynamoDbItemEncryptorConfig(nativeConfig)
-        .build();
+      final DynamoDbItemEncryptor itemEncryptor =
+        DynamoDbItemEncryptor.builder()
+          .DynamoDbItemEncryptorConfig(nativeConfig)
+          .build();
 
-      final TestDynamoDbItemEncryptor wrappedEncryptor = TestDynamoDbItemEncryptor
-        .builder()
-        .impl(itemEncryptor)
-        .build();
+      final TestDynamoDbItemEncryptor wrappedEncryptor =
+        TestDynamoDbItemEncryptor.builder().impl(itemEncryptor).build();
 
       return Result.create_Success(wrappedEncryptor);
     } catch (Exception e) {
