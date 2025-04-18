865171030daa1ca2ea5fe3b7364c7050e7d9dc83
Lucas McDonald <lucmcdon@amazon.com>
2025-04-18 09:26:59 -0700

    m

--- TestVectors/runtimes/rust/src/create_wrapped_item_encryptor.rs
+++ TestVectors/runtimes/rust/src/create_wrapped_item_encryptor.rs
@@ -6,6 +6,7 @@ use dafny_runtime::Object;
 use crate::implementation_from_dafny::software::amazon::cryptography::dbencryptionsdk::dynamodb::itemencryptor::internaldafny::types::IDynamoDbItemEncryptorClient;
 use crate::implementation_from_dafny::software::amazon::cryptography::dbencryptionsdk::dynamodb::itemencryptor::internaldafny::types::Error;
 use crate::implementation_from_dafny::_Wrappers_Compile;
+use aws_db_esdk::item_encryptor::client as item_encryptor_client;
 
 pub mod _CreateWrappedItemEncryptor_Compile {
     pub struct _default {}
@@ -15,11 +16,11 @@ impl _CreateWrappedItemEncryptor_Compile::_default {
     pub fn CreateWrappedItemEncryptor(
         config: &Rc<crate::implementation_from_dafny::software::amazon::cryptography::dbencryptionsdk::dynamodb::itemencryptor::internaldafny::types::DynamoDbItemEncryptorConfig>
     ) -> Rc<_Wrappers_Compile::Result<Object<dyn IDynamoDbItemEncryptorClient>, Rc<Error>>> {
-        let native_config = crate::deps::aws_cryptography_dbEncryptionSdk_dynamoDb_itemencryptor::conversions::dynamo_db_item_encryptor_config::_dynamo_db_item_encryptor_config::plain_from_dafny(config);
+        let native_config = crate::deps::aws_cryptography_dbEncryptionSdk_dynamoDb_itemEncryptor::conversions::dynamo_db_item_encryptor_config::_dynamo_db_item_encryptor_config::plain_from_dafny(config);
         
-        let item_encryptor = aws_db_esdk::item_encryptor::Client::from_conf(native_config);
-        let wrapped_encryptor = crate::deps::com_amazonaws_dynamodb::item_encryptor::DynamoDbItemEncryptorShim { inner: item_encryptor };
-        let dafny_encryptor = ::dafny_runtime::upcast_object()(::dafny_runtime::object::new(wrapped_encryptor));
+        let item_encryptor = item_encryptor_client::Client::from_conf(native_config);
+        let wrapped_encryptor = // where is this generated?
+        let dafny_encryptor = // tood find
         
         Rc::new(crate::r#_Wrappers_Compile::Result::Success {
             value: dafny_encryptor,
