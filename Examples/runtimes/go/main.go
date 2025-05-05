06906958fbb1e4084f752e933970926dd3e60c84
rishav-karanjit <rishavkj@amazon.com>
2025-05-05 16:24:25 -0400

    Item encryptor example from main

--- Examples/runtimes/go/main.go
+++ Examples/runtimes/go/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"github.com/aws/aws-database-encryption-sdk-dynamodb/examples/itemencryptor"
 	"github.com/aws/aws-database-encryption-sdk-dynamodb/examples/keyring"
+	"github.com/aws/aws-database-encryption-sdk-dynamodb/examples/misc"
 	"github.com/aws/aws-database-encryption-sdk-dynamodb/examples/utils"
 )
 
 func main() {
 	keyring.AwsKmsKeyringExample(utils.KmsKeyID(), utils.DdbTableName())
 	keyring.RawAesExample(utils.DdbTableName(), utils.KeyNamespace(), utils.KeyName(), utils.GenerateAes256KeyBytes())
+	itemencryptor.ItemEncryptDecryptExample(utils.KmsKeyID(), utils.DdbTableName())
+	misc.GetEncryptedDataKeyDescriptionExample(utils.KmsKeyID(), utils.DdbTableName())
 }
