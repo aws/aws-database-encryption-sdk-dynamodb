f709b9decf128840bcf4e9530a89a93c72035e43
Rishav karanjit <karanjitrishav4@gmail.com>
2025-05-07 15:56:14 -0400

    refactor(Go): Update examples to use utils and fix var name (#1860)

--- Examples/runtimes/go/utils/exampleUtils.go
+++ Examples/runtimes/go/utils/exampleUtils.go
@@ -3,9 +3,14 @@
 
 package utils
 
+import "crypto/rand"
+
 const (
 	kmsKeyID     = "arn:aws:kms:us-west-2:658956600833:key/b3537ef1-d8dc-4780-9f5a-55776cbb2f7f"
 	ddbTableName = "DynamoDbEncryptionInterceptorTestTableCS"
+	keyNamespace = "my-key-namespace"
+	keyName      = "my-key-name"
+	aesKeyBytes  = 32 // 256 bits = 32 bytes
 )
 
 func KmsKeyID() string {
@@ -14,6 +19,14 @@ func KmsKeyID() string {
 
 func DdbTableName() string {
 	return ddbTableName
+}
+
+func KeyNamespace() string {
+	return keyNamespace
+}
+
+func KeyName() string {
+	return keyName
 }
 
 func AreMapsEqual(map1, map2 map[string]string) bool {
@@ -28,4 +41,21 @@ func AreMapsEqual(map1, map2 map[string]string) bool {
 		}
 	}
 	return true
+}
+
+func HandleError(err error) {
+	// Error handling is limited to panic for demonstration purposes only.
+	// In your code, errors should be properly handled.
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GenerateAes256KeyBytes() []byte {
+	key := make([]byte, aesKeyBytes)
+	// crypto/rand is used here for demonstration.
+	// In your code, you should implement a key generation strategy that meets your security needs.
+	_, err := rand.Read(key)
+	HandleError(err)
+	return key
 }
