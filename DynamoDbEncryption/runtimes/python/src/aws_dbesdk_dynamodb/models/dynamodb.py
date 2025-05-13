b22ddb0e567253ff43aff3249c0db4ca3cbb7b2a
Lucas McDonald <lucmcdon@amazon.com>
2025-05-13 13:13:58 -0700

    m

---
+++ DynamoDbEncryption/runtimes/python/src/aws_dbesdk_dynamodb/models/dynamodb.py
@@ -0,0 +1,3 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+from aws_dbesdk_dynamodb.smithygenerated.aws_cryptography_dbencryptionsdk_dynamodb.models import *
