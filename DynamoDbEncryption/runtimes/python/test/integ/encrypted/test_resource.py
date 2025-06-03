fc5f30c3fff8322ea21277fcf52e9f99ce79a752
Lucas McDonald <lucasmcdonald3@gmail.com>
2025-06-03 09:04:43 -0700

    feat: Python EncryptedResource, EncryptedTablesCollectionManager, and tests (#1904)

---
+++ DynamoDbEncryption/runtimes/python/test/integ/encrypted/test_resource.py
@@ -0,0 +1,201 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+import uuid
+from copy import deepcopy
+
+import boto3
+import pytest
+
+from aws_dbesdk_dynamodb.encrypted.resource import EncryptedResource, EncryptedTablesCollectionManager
+from aws_dbesdk_dynamodb.encrypted.table import EncryptedTable
+
+from ...constants import INTEG_TEST_DEFAULT_DYNAMODB_TABLE_NAME, INTEG_TEST_DEFAULT_TABLE_CONFIGS
+from ...items import (
+    complex_item_ddb,
+    complex_item_dict,
+    complex_key_dict,
+    simple_item_ddb,
+    simple_item_dict,
+    simple_key_dict,
+)
+from ...requests import (
+    basic_batch_get_item_request_dict,
+    basic_batch_write_item_delete_request_dict,
+    basic_batch_write_item_put_request_dict,
+    basic_delete_item_request_ddb,
+)
+
+
+@pytest.fixture(params=[True, False], ids=["encrypted", "plaintext"])
+def encrypted(request):
+    return request.param
+
+
+def plaintext_resource():
+    return boto3.resource("dynamodb")
+
+
+def encrypted_resource():
+    return EncryptedResource(
+        resource=plaintext_resource(),
+        encryption_config=INTEG_TEST_DEFAULT_TABLE_CONFIGS,
+    )
+
+
+@pytest.fixture
+def resource(encrypted):
+    if encrypted:
+        return encrypted_resource()
+    else:
+        return plaintext_resource()
+
+
+@pytest.fixture
+def tables(resource):
+    return resource.tables
+
+
+@pytest.fixture(scope="module")
+def test_run_suffix():
+    return "-" + str(uuid.uuid4())
+
+
+@pytest.fixture
+def test_items(test_run_suffix):
+    items = [deepcopy(complex_item_dict), deepcopy(simple_item_dict)]
+    for item in items:
+        item["partition_key"] += test_run_suffix
+    return items
+
+
+@pytest.fixture
+def test_keys(test_run_suffix):
+    keys = [deepcopy(complex_key_dict), deepcopy(simple_key_dict)]
+    for key in keys:
+        key["partition_key"] += test_run_suffix
+    return keys
+
+
+def test_GIVEN_items_WHEN_batch_write_and_get_THEN_round_trip_passes(
+    resource,
+    test_items,
+    test_keys,
+):
+    batch_write_item_put_request = basic_batch_write_item_put_request_dict(test_items)
+    batch_write_response = resource.batch_write_item(**batch_write_item_put_request)
+    assert batch_write_response["ResponseMetadata"]["HTTPStatusCode"] == 200
+
+    batch_get_item_request = basic_batch_get_item_request_dict(test_keys)
+    batch_get_response = resource.batch_get_item(**batch_get_item_request)
+    assert batch_get_response["ResponseMetadata"]["HTTPStatusCode"] == 200
+    responses = batch_get_response["Responses"][INTEG_TEST_DEFAULT_DYNAMODB_TABLE_NAME]
+    assert len(responses) == 2
+    for response in responses:
+        assert response in test_items
+
+    batch_write_item_delete_request = basic_batch_write_item_delete_request_dict(test_keys)
+    batch_write_response = resource.batch_write_item(**batch_write_item_delete_request)
+    assert batch_write_response["ResponseMetadata"]["HTTPStatusCode"] == 200
+
+    batch_get_response = resource.batch_get_item(**batch_get_item_request)
+    assert batch_get_response["ResponseMetadata"]["HTTPStatusCode"] == 200
+    assert len(batch_get_response["Responses"][INTEG_TEST_DEFAULT_DYNAMODB_TABLE_NAME]) == 0
+
+
+def test_GIVEN_encrypted_resource_WHEN_Table_THEN_returns_encrypted_table_with_correct_arguments():
+    # Given: Encrypted resource
+    resource = encrypted_resource()
+    # When: Table
+    table = resource.Table(INTEG_TEST_DEFAULT_DYNAMODB_TABLE_NAME)
+    # Then: Returns encrypted table
+    assert isinstance(table, EncryptedTable)
+    assert table.name == INTEG_TEST_DEFAULT_DYNAMODB_TABLE_NAME
+    assert table._encryption_config == resource._encryption_config
+
+
+def test_GIVEN_encrypted_resource_WHEN_tables_THEN_returns_encrypted_tables_collection_manager():
+    # Given: Encrypted resource
+    resource = encrypted_resource()
+    # When: tables
+    tables = resource.tables
+    # Then: Returns encrypted tables collection manager
+    assert isinstance(tables, EncryptedTablesCollectionManager)
+    # Given: Encrypted tables collection manager
+    tables = resource.tables
+    # When: all
+    iter = tables.all()
+    tables_list = []
+    for table in iter:
+        tables_list.append(table)
+    assert len(tables_list) > 0
+    for table in tables_list:
+        assert isinstance(table, EncryptedTable)
+        assert table._encryption_config == resource._encryption_config
+
+
+def test_GIVEN_tables_WHEN_all_THEN_returns_tables(
+    tables,
+):
+    # Given: Tables
+    # When: all
+    tables_list = []
+    for table in tables.all():
+        tables_list.append(table)
+    # Then: Returns tables
+    assert len(tables_list) > 0
+    table_names = [table.name for table in tables_list]
+    # "All tables" includes the integ test table
+    assert INTEG_TEST_DEFAULT_DYNAMODB_TABLE_NAME in table_names
+
+
+def test_GIVEN_tables_WHEN_filter_THEN_returns_tables(
+    tables,
+):
+    # Given: Tables
+    # When: filter
+    tables_list = []
+    for table in tables.filter(
+        ExclusiveStartTableName=INTEG_TEST_DEFAULT_DYNAMODB_TABLE_NAME,
+        Limit=1,
+    ):
+        tables_list.append(table)
+    # Then: Returns tables
+    assert len(tables_list) > 0
+    table_names = [table.name for table in tables_list]
+    # The filter request started from the integ test table, not inclusive; it should not be in the list
+    assert INTEG_TEST_DEFAULT_DYNAMODB_TABLE_NAME not in table_names
+
+
+def test_GIVEN_tables_WHEN_limit_THEN_returns_tables(
+    tables,
+):
+    # Given: Tables
+    # When: limit
+    tables_list = []
+    for table in tables.limit(count=1):
+        tables_list.append(table)
+    # Then: Returns tables
+    assert len(tables_list) == 1
+
+
+def test_GIVEN_tables_WHEN_page_size_THEN_returns_tables(
+    tables,
+):
+    # Given: Tables
+    # When: page_size
+    tables_list = []
+    for table in tables.page_size(count=1):
+        tables_list.append(table)
+    # Then: Returns tables
+    assert len(tables_list) > 0
+
+
+# Delete the items in the table after the module runs
+@pytest.fixture(scope="module", autouse=True)
+def cleanup_after_module(test_run_suffix):
+    yield
+    table = boto3.client("dynamodb")
+    items = [deepcopy(simple_item_ddb), deepcopy(complex_item_ddb)]
+    for item in items:
+        item["partition_key"]["S"] += test_run_suffix
+        table.delete_item(**basic_delete_item_request_ddb(item))
---
+++ Examples/runtimes/python/DynamoDBEncryption/src/encrypted_resource/batch_read_write_example.py
@@ -0,0 +1,183 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+"""
+Example for using an EncryptedResource to batch read and write items.
+
+Running this example requires access to the DDB Table whose name
+is provided in the function arguments.
+This table must be configured with the following
+primary key configuration:
+- Partition key is named "partition_key" with type (S)
+- Sort key is named "sort_key" with type (N)
+"""
+
+import boto3
+from aws_cryptographic_material_providers.mpl import AwsCryptographicMaterialProviders
+from aws_cryptographic_material_providers.mpl.config import MaterialProvidersConfig
+from aws_cryptographic_material_providers.mpl.models import (
+    CreateAwsKmsMrkMultiKeyringInput,
+    DBEAlgorithmSuiteId,
+)
+from aws_cryptographic_material_providers.mpl.references import IKeyring
+from aws_dbesdk_dynamodb.encrypted.resource import EncryptedResource
+from aws_dbesdk_dynamodb.structures.dynamodb import (
+    DynamoDbTableEncryptionConfig,
+    DynamoDbTablesEncryptionConfig,
+)
+from aws_dbesdk_dynamodb.structures.structured_encryption import (
+    CryptoAction,
+)
+
+
+def encrypted_resource_batch_read_write_example(
+    kms_key_id: str,
+    dynamodb_table_name: str,
+):
+    """Use an EncryptedResource to batch read and write items."""
+    # 1. Create a Keyring. This Keyring will be responsible for protecting the data keys that protect your data.
+    #    For this example, we will create a AWS KMS Keyring with the AWS KMS Key we want to use.
+    #    We will use the `CreateMrkMultiKeyring` method to create this keyring,
+    #    as it will correctly handle both single region and Multi-Region KMS Keys.
+    mat_prov: AwsCryptographicMaterialProviders = AwsCryptographicMaterialProviders(config=MaterialProvidersConfig())
+    kms_mrk_multi_keyring_input: CreateAwsKmsMrkMultiKeyringInput = CreateAwsKmsMrkMultiKeyringInput(
+        generator=kms_key_id,
+    )
+    kms_mrk_multi_keyring: IKeyring = mat_prov.create_aws_kms_mrk_multi_keyring(input=kms_mrk_multi_keyring_input)
+
+    # 2. Configure which attributes are encrypted and/or signed when writing new items.
+    #    For each attribute that may exist on the items we plan to write to our DynamoDbTable,
+    #    we must explicitly configure how they should be treated during item encryption:
+    #      - ENCRYPT_AND_SIGN: The attribute is encrypted and included in the signature
+    #      - SIGN_ONLY: The attribute not encrypted, but is still included in the signature
+    #      - DO_NOTHING: The attribute is not encrypted and not included in the signature
+    attribute_actions_on_encrypt = {
+        "partition_key": CryptoAction.SIGN_ONLY,
+        "sort_key": CryptoAction.SIGN_ONLY,
+        "attribute1": CryptoAction.ENCRYPT_AND_SIGN,
+        "attribute2": CryptoAction.SIGN_ONLY,
+        ":attribute3": CryptoAction.DO_NOTHING,
+    }
+
+    # 3. Configure which attributes we expect to be included in the signature
+    #    when reading items. There are two options for configuring this:
+    #
+    #    - (Recommended) Configure `allowedUnsignedAttributesPrefix`:
+    #      When defining your DynamoDb schema and deciding on attribute names,
+    #      choose a distinguishing prefix (such as ":") for all attributes that
+    #      you do not want to include in the signature.
+    #      This has two main benefits:
+    #      - It is easier to reason about the security and authenticity of data within your item
+    #        when all unauthenticated data is easily distinguishable by their attribute name.
+    #      - If you need to add new unauthenticated attributes in the future,
+    #        you can easily make the corresponding update to your `attributeActionsOnEncrypt`
+    #        and immediately start writing to that new attribute, without
+    #        any other configuration update needed.
+    #      Once you configure this field, it is not safe to update it.
+    #
+    #    - Configure `allowedUnsignedAttributes`: You may also explicitly list
+    #      a set of attributes that should be considered unauthenticated when encountered
+    #      on read. Be careful if you use this configuration. Do not remove an attribute
+    #      name from this configuration, even if you are no longer writing with that attribute,
+    #      as old items may still include this attribute, and our configuration needs to know
+    #      to continue to exclude this attribute from the signature scope.
+    #      If you add new attribute names to this field, you must first deploy the update to this
+    #      field to all readers in your host fleet before deploying the update to start writing
+    #      with that new attribute.
+    #
+    #   For this example, we have designed our DynamoDb table such that any attribute name with
+    #   the ":" prefix should be considered unauthenticated.
+    unsignAttrPrefix: str = ":"
+
+    # 4. Create the DynamoDb Encryption configuration for the table we will be writing to.
+    table_configs = {}
+    table_config = DynamoDbTableEncryptionConfig(
+        logical_table_name=dynamodb_table_name,
+        partition_key_name="partition_key",
+        sort_key_name="sort_key",
+        attribute_actions_on_encrypt=attribute_actions_on_encrypt,
+        keyring=kms_mrk_multi_keyring,
+        allowed_unsigned_attribute_prefix=unsignAttrPrefix,
+        # Specifying an algorithm suite is not required,
+        # but is done here to demonstrate how to do so.
+        # We suggest using the
+        # `ALG_AES_256_GCM_HKDF_SHA512_COMMIT_KEY_ECDSA_P384_SYMSIG_HMAC_SHA384` suite,
+        # which includes AES-GCM with key derivation, signing, and key commitment.
+        # This is also the default algorithm suite if one is not specified in this config.
+        # For more information on supported algorithm suites, see:
+        #   https://docs.aws.amazon.com/database-encryption-sdk/latest/devguide/supported-algorithms.html
+        algorithm_suite_id=DBEAlgorithmSuiteId.ALG_AES_256_GCM_HKDF_SHA512_COMMIT_KEY_ECDSA_P384_SYMSIG_HMAC_SHA384,
+    )
+    table_configs[dynamodb_table_name] = table_config
+    tables_config = DynamoDbTablesEncryptionConfig(table_encryption_configs=table_configs)
+
+    # 5. Create the EncryptedResource
+    encrypted_resource = EncryptedResource(
+        resource=boto3.resource("dynamodb"),
+        encryption_config=tables_config,
+    )
+
+    # 6. Write a batch of items to the table.
+    #    Before the items get sent to DynamoDb, they will be encrypted
+    #    client-side, according to our configuration.
+    items = [
+        {
+            "partition_key": "PythonEncryptedResourceBatchReadWriteExample1",
+            "sort_key": 0,
+            "attribute1": "encrypt and sign me!",
+            "attribute2": "sign me!",
+            ":attribute3": "ignore me!",
+        },
+        {
+            "partition_key": "PythonEncryptedResourceBatchReadWriteExample2",
+            "sort_key": 0,
+            "attribute1": "encrypt and sign me!",
+            "attribute2": "sign me!",
+            ":attribute3": "ignore me!",
+        },
+    ]
+
+    batch_write_items_put_request = {
+        "RequestItems": {
+            dynamodb_table_name: [{"PutRequest": {"Item": item}} for item in items],
+        },
+    }
+
+    batch_write_items_put_response = encrypted_resource.batch_write_item(**batch_write_items_put_request)
+
+    # Demonstrate that BatchWriteItem succeeded
+    assert batch_write_items_put_response["ResponseMetadata"]["HTTPStatusCode"] == 200
+
+    # 7. Read the items back from the table.
+    #    After the items are retrieved from DynamoDb, but before the EncryptedResource
+    #    returns them to the caller, they will be decrypted client-side according to our configuration.
+    batch_get_items_request = {
+        "RequestItems": {
+            dynamodb_table_name: {
+                "Keys": [{"partition_key": item["partition_key"], "sort_key": item["sort_key"]} for item in items],
+            }
+        },
+    }
+
+    batch_get_items_response = encrypted_resource.batch_get_item(**batch_get_items_request)
+
+    # Demonstrate that BatchGetItem succeeded with the expected result
+    assert batch_get_items_response["ResponseMetadata"]["HTTPStatusCode"] == 200
+    for item in batch_get_items_response["Responses"][dynamodb_table_name]:
+        assert item["attribute1"] == "encrypt and sign me!"
+        assert item["attribute2"] == "sign me!"
+        assert item[":attribute3"] == "ignore me!"
+
+    # 8. Delete the items from the table.
+    batch_write_items_delete_request = {
+        "RequestItems": {
+            dynamodb_table_name: [
+                {"DeleteRequest": {"Key": {"partition_key": item["partition_key"], "sort_key": item["sort_key"]}}}
+                for item in items
+            ],
+        },
+    }
+
+    batch_write_items_delete_response = encrypted_resource.batch_write_item(**batch_write_items_delete_request)
+
+    # Demonstrate that BatchWriteItem succeeded
+    assert batch_write_items_delete_response["ResponseMetadata"]["HTTPStatusCode"] == 200
