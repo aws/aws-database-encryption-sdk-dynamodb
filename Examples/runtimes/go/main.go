37d605332d11a9d86230f8d3f42a6ddf61a88b2d
rishav-karanjit <rishavkj@amazon.com>
2025-06-03 18:18:14 -0700

    comment Examples

--- Examples/runtimes/go/main.go
+++ Examples/runtimes/go/main.go
@@ -1,11 +1,6 @@
 package main
 
-import (
-	"github.com/aws/aws-database-encryption-sdk-dynamodb/examples/keyring"
-	"github.com/aws/aws-database-encryption-sdk-dynamodb/examples/utils"
-)
-
 func main() {
-	keyring.AwsKmsKeyringExample(utils.KmsKeyID(), utils.DdbTableName())
-	keyring.RawAesExample(utils.DdbTableName(), utils.KeyNamespace(), utils.KeyName(), utils.GenerateAes256KeyBytes())
+	// keyring.AwsKmsKeyringExample(utils.KmsKeyID(), utils.DdbTableName())
+	// keyring.RawAesExample(utils.DdbTableName(), utils.KeyNamespace(), utils.KeyName(), utils.GenerateAes256KeyBytes())
 }
