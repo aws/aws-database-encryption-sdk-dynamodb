b5325643a906f8ba03ce1dbe4c41311d718877b7
Rishav karanjit <karanjitrishav4@gmail.com>
2025-06-11 09:26:29 -0700

    chore(java): Parallelize Java Examples (#1940)

--- Examples/runtimes/java/Migration/PlaintextToAWSDBE/src/test/java/software/amazon/cryptography/examples/migration/plaintext/TestMigrationExampleStep0.java
+++ Examples/runtimes/java/Migration/PlaintextToAWSDBE/src/test/java/software/amazon/cryptography/examples/migration/plaintext/TestMigrationExampleStep0.java
@@ -2,10 +2,13 @@ package software.amazon.cryptography.examples.migration.plaintext;
 
 import static org.testng.Assert.assertThrows;
 
+import java.util.Arrays;
+import java.util.List;
 import org.testng.annotations.Test;
 import software.amazon.cryptography.examples.awsdbe.MigrationExampleStep1;
 import software.amazon.cryptography.examples.awsdbe.MigrationExampleStep2;
 import software.amazon.cryptography.examples.awsdbe.MigrationExampleStep3;
+import software.amazon.cryptography.examples.migration.TestUtils;
 import software.amazon.cryptography.examples.plaintext.MigrationExampleStep0;
 
 public class TestMigrationExampleStep0 {
@@ -13,28 +16,42 @@ public class TestMigrationExampleStep0 {
   @Test
   public void TestMigrationStep0() {
     // Successfully executes step 0
-    MigrationExampleStep0.MigrationStep0(TestUtils.TEST_DDB_TABLE_NAME, 0);
+    MigrationExampleStep0.MigrationStep0(
+      TestUtils.TEST_DDB_TABLE_NAME,
+      0,
+      TestUtils.PARTITION_KEY
+    );
 
     // Given: Step 1 has succeeded
     MigrationExampleStep1.MigrationStep1(
       TestUtils.TEST_KMS_KEY_ID,
       TestUtils.TEST_DDB_TABLE_NAME,
-      1
+      1,
+      TestUtils.PARTITION_KEY
     );
     // When: Execute Step 0 with sortReadValue=1, Then: Success (i.e. can read plaintext values)
-    MigrationExampleStep0.MigrationStep0(TestUtils.TEST_DDB_TABLE_NAME, 1);
+    MigrationExampleStep0.MigrationStep0(
+      TestUtils.TEST_DDB_TABLE_NAME,
+      1,
+      TestUtils.PARTITION_KEY
+    );
 
     // Given: Step 2 has succeeded
     MigrationExampleStep2.MigrationStep2(
       TestUtils.TEST_KMS_KEY_ID,
       TestUtils.TEST_DDB_TABLE_NAME,
-      2
+      2,
+      TestUtils.PARTITION_KEY
     );
     // When: Execute Step 0 with sortReadValue=2, Then: throws AssertionError (i.e. cannot read encrypted values)
     assertThrows(
       AssertionError.class,
       () -> {
-        MigrationExampleStep0.MigrationStep0(TestUtils.TEST_DDB_TABLE_NAME, 2);
+        MigrationExampleStep0.MigrationStep0(
+          TestUtils.TEST_DDB_TABLE_NAME,
+          2,
+          TestUtils.PARTITION_KEY
+        );
       }
     );
 
@@ -42,14 +59,29 @@ public class TestMigrationExampleStep0 {
     MigrationExampleStep3.MigrationStep3(
       TestUtils.TEST_KMS_KEY_ID,
       TestUtils.TEST_DDB_TABLE_NAME,
-      3
+      3,
+      TestUtils.PARTITION_KEY
     );
     // When: Execute Step 0 with sortReadValue=3, Then: throws AssertionError (i.e. cannot read encrypted values)
     assertThrows(
       AssertionError.class,
       () -> {
-        MigrationExampleStep0.MigrationStep0(TestUtils.TEST_DDB_TABLE_NAME, 3);
+        MigrationExampleStep0.MigrationStep0(
+          TestUtils.TEST_DDB_TABLE_NAME,
+          3,
+          TestUtils.PARTITION_KEY
+        );
       }
     );
+    List<String> sortkeys = Arrays.asList("0", "1", "2", "3");
+    for (String sortkey : sortkeys) {
+      TestUtils.cleanUpDDBItem(
+        TestUtils.TEST_DDB_TABLE_NAME,
+        "partition_key",
+        "sort_key",
+        TestUtils.PARTITION_KEY,
+        sortkey
+      );
+    }
   }
 }
