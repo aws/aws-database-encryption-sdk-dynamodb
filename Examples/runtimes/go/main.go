60b024823ecc9dbabd36674910613ceb080b72fd
rishav-karanjit <rishavkj@amazon.com>
2025-06-26 14:24:57 -0700

    auto commit

--- Examples/runtimes/go/main.go
+++ Examples/runtimes/go/main.go
@@ -11,10 +11,41 @@ import (
 )
 
 func main() {
-	keyring.AwsKmsKeyringExample(utils.KmsKeyID(), utils.DdbTableName())
-	keyring.RawAesExample(utils.DdbTableName(), utils.KeyNamespace(), utils.KeyName(), utils.GenerateAes256KeyBytes())
-	itemencryptor.ItemEncryptDecryptExample(utils.KmsKeyID(), utils.DdbTableName())
-	misc.GetEncryptedDataKeyDescriptionExample(utils.KmsKeyID(), utils.DdbTableName())
-	misc.MultiPutGetExample(utils.KmsKeyID(), utils.DdbTableName())
-	misc.CreateBranchKeyIDExample(utils.TestKeystoreName(), utils.TestLogicalKeystoreName(), utils.TestKeystoreKmsKeyId())
+	// misc examples
+	misc.GetEncryptedDataKeyDescriptionExample(
+		utils.KmsKeyID(),
+		utils.DdbTableName())
+	misc.MultiPutGetExample(
+		utils.KmsKeyID(),
+		utils.DdbTableName())
+	branchKey1 := misc.CreateBranchKeyIDExample(
+		utils.TestKeystoreName(),
+		utils.TestLogicalKeystoreName(),
+		utils.TestKeystoreKmsKeyId())
+	branchKey2 := misc.CreateBranchKeyIDExample(
+		utils.TestKeystoreName(),
+		utils.TestLogicalKeystoreName(),
+		utils.TestKeystoreKmsKeyId())
+
+	// keyring examples
+	keyring.AwsKmsKeyringExample(
+		utils.KmsKeyID(),
+		utils.DdbTableName())
+	keyring.RawAesExample(
+		utils.DdbTableName(),
+		utils.KeyNamespace(),
+		utils.KeyName(),
+		utils.GenerateAes256KeyBytes())
+	keyring.HierarchicalKeyringExample(
+		utils.DdbTableName(),
+		branchKey1,
+		branchKey2,
+		utils.TestKeystoreName(),
+		utils.TestLogicalKeystoreName(),
+		utils.TestKeystoreKmsKeyId())
+
+	// item encryptor example
+	itemencryptor.ItemEncryptDecryptExample(
+		utils.KmsKeyID(),
+		utils.DdbTableName())
 }
