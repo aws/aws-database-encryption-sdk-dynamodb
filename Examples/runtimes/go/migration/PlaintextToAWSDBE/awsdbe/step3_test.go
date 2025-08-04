1d943e1f039c6701f2e7896d685f612cd9048df4
rishav-karanjit <rishavkj@amazon.com>
2025-08-04 12:05:34 -0700

    auto commit

--- Examples/runtimes/go/migration/PlaintextToAWSDBE/awsdbe/step3_test.go
+++ Examples/runtimes/go/migration/PlaintextToAWSDBE/awsdbe/step3_test.go
@@ -15,23 +15,32 @@ func TestMigrationStep3(t *testing.T) {
 	sortKeys := []string{"0", "1", "2", "3"}
 
 	// Successfully executes Step 3
-	utils.HandleError(MigrationStep3(kmsKeyID, tableName, partitionKey, sortKeys[3]))
+	err := MigrationStep3(kmsKeyID, tableName, partitionKey, sortKeys[3])
+	utils.HandleError(err)
 
 	// Given: Step 0 has succeeded
-	utils.HandleError(plaintext.MigrationStep0(tableName, partitionKey, sortKeys[0]))
+	err = plaintext.MigrationStep0(tableName, partitionKey, sortKeys[0])
+	utils.HandleError(err)
+
 	// When: Execute Step 3 with sortReadValue=0, Then: should panic (cannot read plaintext values)
 	// MigrationStep3(kmsKeyID, tableName, partitionKey, sortKeys[0])
 	utils.AssertServiceError(MigrationStep3(kmsKeyID, tableName, partitionKey, sortKeys[0]), "DynamoDB", "GetItem", "Encrypted item missing expected header and footer attributes")
 
 	// Given: Step 1 has succeeded
-	utils.HandleError(MigrationStep1(kmsKeyID, tableName, partitionKey, sortKeys[1]))
-	// When: Execute Step 3 with sortReadValue=1, Then: should panic (cannot read plaintext values)
-	utils.AssertServiceError(MigrationStep3(kmsKeyID, tableName, partitionKey, sortKeys[1]), "DynamoDB", "GetItem", "Encrypted item missing expected header and footer attributes")
+	err = MigrationStep1(kmsKeyID, tableName, partitionKey, sortKeys[1])
+	utils.HandleError(err)
+
+	// When: Execute Step 3 with sortReadValue=1, Then: should error out (cannot read plaintext values)
+	err = MigrationStep3(kmsKeyID, tableName, partitionKey, sortKeys[1])
+	utils.AssertServiceError(err, "DynamoDB", "GetItem", "Encrypted item missing expected header and footer attributes")
 
 	// Given: Step 2 has succeeded
-	utils.HandleError(MigrationStep2(kmsKeyID, tableName, partitionKey, sortKeys[2]))
+	err = MigrationStep2(kmsKeyID, tableName, partitionKey, sortKeys[2])
+	utils.HandleError(err)
+
 	// When: Execute Step 3 with sortReadValue=2, Then: Success (can read encrypted values)
-	utils.HandleError(MigrationStep3(kmsKeyID, tableName, partitionKey, sortKeys[2]))
+	err = MigrationStep3(kmsKeyID, tableName, partitionKey, sortKeys[2])
+	utils.HandleError(err)
 
 	// Cleanup
 	for _, sortKey := range sortKeys {
