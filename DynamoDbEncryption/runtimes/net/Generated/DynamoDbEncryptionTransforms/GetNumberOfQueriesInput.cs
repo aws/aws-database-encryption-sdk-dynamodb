ef344d910cbf0b0a6307e6300431219128442dcc
Andy Jewell <ajewell@amazon.com>
2025-08-11 13:42:27 -0400

    m

--- DynamoDbEncryption/runtimes/net/Generated/DynamoDbEncryptionTransforms/GetNumberOfQueriesInput.cs
+++ DynamoDbEncryption/runtimes/net/Generated/DynamoDbEncryptionTransforms/GetNumberOfQueriesInput.cs
@@ -2,8 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 // Do not modify this file. This file is machine generated, and any changes to it will be overwritten.
 using System;
-using AWS.Cryptography.DbEncryptionSDK.DynamoDb;
-namespace AWS.Cryptography.DbEncryptionSDK.DynamoDb
+using AWS.Cryptography.DbEncryptionSDK.DynamoDb.Transforms;
+namespace AWS.Cryptography.DbEncryptionSDK.DynamoDb.Transforms
 {
   public class GetNumberOfQueriesInput
   {
