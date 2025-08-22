f2f1a39a4622412075430114ad70dbd71dbba36f
rishav-karanjit <rishavkj@amazon.com>
2025-08-22 11:41:41 -0700

    auto commit

--- Examples/runtimes/net/src/migration/PlaintextToAWSDBE/awsdbe/MigrationStep3Test.cs
+++ Examples/runtimes/net/src/migration/PlaintextToAWSDBE/awsdbe/MigrationStep3Test.cs
@@ -21,31 +21,31 @@ namespace Examples.migration.PlaintextToAWSDBE.awsdbe
             string partitionKey = Guid.NewGuid().ToString();
             string[] sortKeys = { "0", "1", "2", "3" };
 
-            // Given: Step 0 has succeeded
-            bool success = await MigrationStep0.MigrationStep0Example(tableName, partitionKey, sortKeys[0], sortKeys[0]);
-            Assert.True(success, "MigrationStep0 should complete successfully");
-
-            // Given: Step 1 has succeeded
-            success = await MigrationStep1.MigrationStep1Example(kmsKeyID, tableName, partitionKey, sortKeys[1], sortKeys[1]);
-            Assert.True(success, "MigrationStep1 should complete successfully");
-
-            // Given: Step 2 has succeeded
-            success = await MigrationStep2.MigrationStep2Example(kmsKeyID, tableName, partitionKey, sortKeys[2], sortKeys[2]);
-            Assert.True(success, "MigrationStep2 should complete successfully");
-
             // Successfully executes step 3
             success = await MigrationStep3.MigrationStep3Example(kmsKeyID, tableName, partitionKey, sortKeys[3], sortKeys[3]);
             Assert.True(success, "MigrationStep3 should complete successfully");
+
+            // Given: Step 0 has succeeded
+            bool success = await MigrationStep0.MigrationStep0Example(tableName, partitionKey, sortKeys[0], sortKeys[0]);
+            Assert.True(success, "MigrationStep0 should complete successfully");
 
             // When: Execute Step 3 with sortReadValue=0, Then: should error out when reading plaintext items from Step 0
             var exception = await Assert.ThrowsAsync<DynamoDbItemEncryptorException>(() =>
                 MigrationStep3.MigrationStep3Example(kmsKeyID, tableName, partitionKey, sortKeys[3], sortKeys[0]));
 
-            Assert.Contains("encrypted item missing expected header and footer attributes", exception.Message.ToLower());
+            // Given: Step 1 has succeeded
+            success = await MigrationStep1.MigrationStep1Example(kmsKeyID, tableName, partitionKey, sortKeys[1], sortKeys[1]);
+            Assert.True(success, "MigrationStep1 should complete successfully");
 
             // When: Execute Step 3 with sortReadValue=1, Then: should error out when reading plaintext items from Step 1
             exception = await Assert.ThrowsAsync<DynamoDbItemEncryptorException>(() =>
                 MigrationStep3.MigrationStep3Example(kmsKeyID, tableName, partitionKey, sortKeys[3], sortKeys[1]));
+            Assert.Contains("encrypted item missing expected header and footer attributes", exception.Message.ToLower());
+
+            // Given: Step 2 has succeeded
+            success = await MigrationStep2.MigrationStep2Example(kmsKeyID, tableName, partitionKey, sortKeys[2], sortKeys[2]);
+            Assert.True(success, "MigrationStep2 should complete successfully");
+
             Assert.Contains("encrypted item missing expected header and footer attributes", exception.Message.ToLower());
 
             // When: Execute Step 3 with sortReadValue=2, Then: Success (i.e. can read encrypted values from Step 2)
