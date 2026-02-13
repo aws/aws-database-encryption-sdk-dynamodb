aa21fda7e25ca17e5c97d5ab5b3779d2d6db7df8
Lucas McDonald <lucmcdon@amazon.com>
2026-02-13 10:49:52 -0800

    m

--- DynamoDbEncryption/runtimes/rust/examples/main.rs
+++ DynamoDbEncryption/runtimes/rust/examples/main.rs
@@ -48,37 +48,37 @@ impl<T: std::fmt::Debug> From<T> for BoxError {
 #[tokio::main]
 pub async fn main() -> Result<(), BoxError2> {
     basic_get_put_example::put_item_get_item().await?;
-    // itemencryptor::item_encrypt_decrypt::encrypt_decrypt().await?;
-    // get_encrypted_data_key_description::get_encrypted_data_key_description().await?;
-    // multi_get_put_example::multi_put_get().await?;
-    // keyring::raw_rsa_keyring::put_item_get_item().await?;
-    // keyring::kms_rsa_keyring::put_item_get_item().await?;
-    // keyring::multi_mrk_keyring::put_item_get_item().await?;
-    // keyring::raw_aes_keyring::put_item_get_item().await?;
-    // keyring::multi_keyring::put_item_get_item().await?;
-    // keyring::mrk_discovery_multi_keyring::put_item_get_item().await?;
-    // clientsupplier::client_supplier_example::put_item_get_item().await?;
+    itemencryptor::item_encrypt_decrypt::encrypt_decrypt().await?;
+    get_encrypted_data_key_description::get_encrypted_data_key_description().await?;
+    multi_get_put_example::multi_put_get().await?;
+    keyring::raw_rsa_keyring::put_item_get_item().await?;
+    keyring::kms_rsa_keyring::put_item_get_item().await?;
+    keyring::multi_mrk_keyring::put_item_get_item().await?;
+    keyring::raw_aes_keyring::put_item_get_item().await?;
+    keyring::multi_keyring::put_item_get_item().await?;
+    keyring::mrk_discovery_multi_keyring::put_item_get_item().await?;
+    clientsupplier::client_supplier_example::put_item_get_item().await?;
 
-    // let key_id = create_keystore_key::keystore_create_key().await?;
-    // let key_id2 = create_keystore_key::keystore_create_key().await?;
-    // // Key creation is eventually consistent, so wait 5 seconds to decrease the likelihood
-    // // our test fails due to eventual consistency issues.
-    // println!("Key Store Keys created. Waiting 5 seconds for consistency.");
-    // std::thread::sleep(std::time::Duration::from_secs(5));
+    let key_id = create_keystore_key::keystore_create_key().await?;
+    let key_id2 = create_keystore_key::keystore_create_key().await?;
+    // Key creation is eventually consistent, so wait 5 seconds to decrease the likelihood
+    // our test fails due to eventual consistency issues.
+    println!("Key Store Keys created. Waiting 5 seconds for consistency.");
+    std::thread::sleep(std::time::Duration::from_secs(5));
 
-    // keyring::hierarchical_keyring::put_item_get_item(&key_id, &key_id2).await?;
+    keyring::hierarchical_keyring::put_item_get_item(&key_id, &key_id2).await?;
 
-    // searchableencryption::basic_searchable_encryption::put_and_query_with_beacon(&key_id).await?;
-    // searchableencryption::beacon_styles_searchable_encryption::put_and_query_with_beacon(&key_id)
-    //     .await?;
-    // searchableencryption::compound_beacon_searchable_encryption::put_and_query_with_beacon(&key_id)
-    //     .await?;
-    // searchableencryption::virtual_beacon_searchable_encryption::put_and_query_with_beacon(&key_id)
-    //     .await?;
-    // searchableencryption::complexexample::complex_searchable_encryption::run_example(&key_id)
-    //     .await?;
+    searchableencryption::basic_searchable_encryption::put_and_query_with_beacon(&key_id).await?;
+    searchableencryption::beacon_styles_searchable_encryption::put_and_query_with_beacon(&key_id)
+        .await?;
+    searchableencryption::compound_beacon_searchable_encryption::put_and_query_with_beacon(&key_id)
+        .await?;
+    searchableencryption::virtual_beacon_searchable_encryption::put_and_query_with_beacon(&key_id)
+        .await?;
+    searchableencryption::complexexample::complex_searchable_encryption::run_example(&key_id)
+        .await?;
 
-    // // ScanError will have to wait until we have a reasonable error message strategy
+    // ScanError will have to wait until we have a reasonable error message strategy
 
     println!("All examples completed successfully.\n");
     Ok(())
