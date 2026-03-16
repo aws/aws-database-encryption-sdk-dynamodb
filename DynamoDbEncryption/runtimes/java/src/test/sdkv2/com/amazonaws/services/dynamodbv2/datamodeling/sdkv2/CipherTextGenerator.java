7a434fc9c6e7fa4263072566300abc42557bb143
rishav-karanjit <rishavkj@amazon.com>
2026-03-16 11:06:33 -0700

    Rename CipherTestGenerator to CipherTextGenerator

--- DynamoDbEncryption/runtimes/java/src/test/sdkv2/com/amazonaws/services/dynamodbv2/datamodeling/sdkv2/CipherTextGenerator.java
+++ DynamoDbEncryption/runtimes/java/src/test/sdkv2/com/amazonaws/services/dynamodbv2/datamodeling/sdkv2/CipherTextGenerator.java
@@ -11,7 +11,7 @@ import java.util.Collections;
 import java.util.Map;
 import org.testng.annotations.Test;
 
-public class CipherTestGenerator extends HolisticIT {
+public class CipherTextGenerator extends HolisticIT {
 
   @Test(enabled = false)
   public void generateKmsAes128Vector() throws IOException {
