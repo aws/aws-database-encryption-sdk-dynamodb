b76650ec18b83005f52c3bcb5ab372598ea7d084
Jose Corella <jocorell@amazon.com>
2026-06-29 17:00:38 -0700

    docs: For release 4.1.0

--- DynamoDbEncryption/runtimes/java/src/main/dafny-generated/software/amazon/cryptography/dbencryptionsdk/dynamodb/internaldafny/types/ILegacyDynamoDbEncryptor.java
+++
@@ -1,7 +0,0 @@
-// Interface ILegacyDynamoDbEncryptor
-// Dafny trait ILegacyDynamoDbEncryptor compiled into Java
-package software.amazon.cryptography.dbencryptionsdk.dynamodb.internaldafny.types;
-
-@SuppressWarnings({"unchecked", "deprecation"})
-public interface ILegacyDynamoDbEncryptor {
-}
--- DynamoDbEncryption/runtimes/java/src/main/dafny-generated/software/amazon/cryptography/dbencryptionsdk/dynamodb/itemencryptor/internaldafny/types/IDynamoDbItemEncryptorClient.java
+++
@@ -1,9 +0,0 @@
-// Interface IDynamoDbItemEncryptorClient
-// Dafny trait IDynamoDbItemEncryptorClient compiled into Java
-package software.amazon.cryptography.dbencryptionsdk.dynamodb.itemencryptor.internaldafny.types;
-
-@SuppressWarnings({"unchecked", "deprecation"})
-public interface IDynamoDbItemEncryptorClient {
-  public Wrappers_Compile.Result<EncryptItemOutput, Error> EncryptItem(EncryptItemInput input);
-  public Wrappers_Compile.Result<DecryptItemOutput, Error> DecryptItem(DecryptItemInput input);
-}
